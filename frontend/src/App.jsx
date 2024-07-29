392a63bc749035328641bae69d7169654b2ee127
akash <akash.royartist@gmail.com>
2024-07-29 22:06:53 +0530

    part: 2 - added routing to login/signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,36 @@
 import "./App.css";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/Signup",
+    element: <SignUpPage />,
+  },
+]);
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <RouterProvider router={router} />
+      {/* <SignUpPage /> */}
+      {/* <LoginPage/> */}
+      {/* <Home/> */}
     </div>
   );
 };
