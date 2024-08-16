4ca1b26e43f26dd1e04c18e8692dc4e1c685998e
akash <akash.royartist@gmail.com>
2024-08-16 08:51:50 +0530

    Part-5: Made changes in the product list, and added product detail page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import CartPage from "./pages/CartPage";
 import Checkout from "./pages/Checkout";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 import SignUpPage from "./pages/SignUpPage";
 import {
   createBrowserRouter,
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
   {
     path: "/checkout",
     element: <Checkout />,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage/>,
   },
 ]);
 
---
+++ frontend/src/pages/ProductDetailPage.jsx
@@ -0,0 +1,13 @@
+import ProductDetail from "../features/product-list/components/ProductDetail";
+import Navbar from "../features/navbar/Navbar";
+
+const ProductDetailPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <ProductDetail />
+    </div>
+  );
+};
+
+export default ProductDetailPage;
